39c9628a0b7d7575074fce9e8642d72d3959752d
EstebanBedoyaHiguita <estebanbedoyahiguita@gmail.com>
2024-04-05 20:45:32 -0500

    Cambio de carpeta de vuelos y hospital

--- vuelos/src/Main.java
+++ vuelos/src/Main.java
@@ -1,9 +1,5 @@
-import Data_base.ConfiDB;
-
-import java.sql.Connection;
-
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello world!");
     }
 }
